09b7d95943643f78d5637b89637b061569477348
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2018-12-28 15:06:44 +0100

    Deliver emails to bucket

--- examples/email-hook/bin/email-hook.ts
+++ examples/email-hook/bin/email-hook.ts
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 import cdk = require("@aws-cdk/cdk");
+import { EmailBucketStack } from "../lib/email-bucket-stack";
 import { EmailHookStack } from "../lib/email-hook-stack";
 
 const app = new cdk.App();
-new EmailHookStack(app, "EmailHookStack");
+
+const emailBucketStack = new EmailBucketStack(
+  app,
+  "EmailBucketStack",
+  "inbound-emails"
+);
+
+new EmailHookStack(app, "EmailHookStack", {
+  bucketRef: emailBucketStack.bucketRef,
+  emails: ["foo@example.org"]
+});
+
 app.run();
--- examples/email-hook/lib/email-hook-stack.ts
+++ examples/email-hook/lib/email-hook-stack.ts
@@ -1,15 +1,51 @@
 import cdk = require("@aws-cdk/cdk");
-import { CfnReceiptRuleSet } from "@aws-cdk/aws-ses";
+import { CfnReceiptRuleSet, CfnReceiptRule } from "@aws-cdk/aws-ses";
 import { CdkActivateSesRuleSet } from "cdk-activate-ses-rule-set";
+import s3 = require("@aws-cdk/aws-s3");
+
+interface EmailHookStackProps {
+  bucketRef: s3.BucketRefProps;
+  emails: string[];
+}
 
 export class EmailHookStack extends cdk.Stack {
-  constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
-    super(parent, name, props);
+  constructor(parent: cdk.App, name: string, props: EmailHookStackProps) {
+    super(parent, name);
 
+    const bucket = s3.Bucket.import(this, "EmailBucket", props.bucketRef);
     const inboundRuleSet = new CfnReceiptRuleSet(this, `InboundRuleSet`);
+
+    props.emails.forEach(email => {
+      this.inboundEmailAddress(email, inboundRuleSet, bucket);
+    });
 
     new CdkActivateSesRuleSet(this, "ActivateSesRuleSet", {
       inboundRuleSet
+    });
+  }
+
+  inboundEmailAddress(
+    emailAddress: string,
+    inboundRuleSet: CfnReceiptRuleSet,
+    bucket: s3.BucketRef
+  ): void {
+    const name = emailAddress.split("@")[0].replace("-", "");
+    new CfnReceiptRule(this, `Rule${name}`, {
+      rule: {
+        actions: [
+          {
+            s3Action: {
+              bucketName: bucket.bucketName,
+              objectKeyPrefix: name
+            }
+          }
+        ],
+        enabled: true,
+        name: name,
+        recipients: [emailAddress],
+        scanEnabled: true
+      },
+      ruleSetName: inboundRuleSet.receiptRuleSetName
     });
   }
 }
