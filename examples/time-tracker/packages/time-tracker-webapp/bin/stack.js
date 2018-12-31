dac8f49b8127de1461710f0876c2a2ba06c3259d
Mike Bild <mike.bild@googlemail.com>
2018-12-31 11:28:21 +0100

    refactor(time-tracker-webapp): add CloudFront and Route53 Alias

--- examples/time-tracker/packages/time-tracker-webapp/bin/stack.js
+++ examples/time-tracker/packages/time-tracker-webapp/bin/stack.js
@@ -2,12 +2,14 @@ const { join } = require('path');
 const { Stack, RemovalPolicy } = require('@aws-cdk/cdk');
 const { Bucket } = require('@aws-cdk/aws-s3');
 const { BucketDeployment, Source } = require('@aws-cdk/aws-s3-deployment');
+const { HostedZoneProvider, AliasRecord } = require('@aws-cdk/aws-route53');
+const { CloudFrontWebDistribution } = require('@aws-cdk/aws-cloudfront');
 
 module.exports = class WebApp extends Stack {
   constructor(parent, id, props) {
     super(parent, id, props);
 
-    const websiteBucket = new Bucket(this, 'time-tracker-webapp', {
+    const websiteBucket = new Bucket(this, 'BucketWebsite', {
       bucketName           : 'time-tracker-webapp.mikebild.com',
       websiteIndexDocument : 'index.html',
       websiteErrorDocument : 'error.html',
@@ -15,11 +17,36 @@ module.exports = class WebApp extends Stack {
       removalPolicy        : RemovalPolicy.Destroy,
       retainOnDelete       : false,
     });
-    websiteBucket.grantPublicAccess('*', 's3:GetObject');
 
     const deployment = new BucketDeployment(this, 'DeployWebsite', {
       source            : Source.asset(join(__dirname, '../dist')),
       destinationBucket : websiteBucket,
+    });
+
+    const distribution = new CloudFrontWebDistribution(
+      this,
+      'DistributionWebSite',
+      {
+        originConfigs : [
+          {
+            s3OriginSource : {
+              s3BucketSource : websiteBucket,
+            },
+            behaviors      : [
+              { isDefaultBehavior: true },
+            ],
+          },
+        ],
+      }
+    );
+
+    const zone = new HostedZoneProvider(this, {
+      domainName : 'mikebild.com',
+    }).findAndImport(this, 'Zone');
+
+    new AliasRecord(zone, 'WebsiteAliasRecord', {
+      recordName : 'time-tracker-webapp.mikebild.com',
+      target     : distribution,
     });
   }
 };
