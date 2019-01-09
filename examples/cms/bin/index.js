f2026642c08c82de4688cfb5b55335d8d2018cee
Mike Bild <mike.bild@googlemail.com>
2019-01-09 06:17:24 +0100

    fix(cms-example): DNS Alias

--- examples/cms/bin/index.js
+++ examples/cms/bin/index.js
@@ -1,16 +1,21 @@
 const { App } = require('@aws-cdk/cdk');
 
+const CMSUserProfile = require('../packages/cms-user-profile/bin/stack');
 const CMSCatalog = require('../packages/cms-catalog/bin/stack');
 const CMSGraphQLApi = require('../packages/cms-graphql-api/bin/stack');
 const CMSSiteGenerator = require('../packages/cms-site-generator/bin/stack');
 const CMSWebApp = require('../packages/cms-webapp/bin/stack');
+const CMSWebAppDNSAlias = require('../packages/cms-webapp/bin/dns-alias');
 
 const app = new App();
 
-// Data Catalog
+// Data Catalog (Articles, etc.)
 const catalog = new CMSCatalog(app, 'cms-catalog');
 
-// Preview and Release WebSite Generation
+// User Profile
+const userProfile = new CMSUserProfile(app, 'cms-user-profile');
+
+// WebSite Generation (Article- Preview/Release)
 const siteGenerator = new CMSSiteGenerator(app, 'cms-site-generator', {
   catalogBucket : catalog.catalogBucket,
 });
@@ -23,9 +28,20 @@ const graphQlApi = new CMSGraphQLApi(app, 'cms-graphql-api', {
 });
 
 // CMS WebApp
-new CMSWebApp(app, 'cms-webapp', {
+const webapp = new CMSWebApp(app, 'cms-webapp', {
   graphQlApiKey : graphQlApi.graphQlApiKey,
   graphQlApiUrl : graphQlApi.graphQlApiUrl,
+  domainName    : 'mikebild.com',
+  hostName      : 'cms-webapp',
+  stageName     : 'prod',
+});
+
+// CMS WebApp DNS Alias
+new CMSWebAppDNSAlias(app, 'cms-webapp-dns-alias', {
+  domainNameRegionalHostedZoneId : webapp.domainNameRegionalHostedZoneId,
+  domainNameRegionalDomainName   : webapp.domainNameRegionalDomainName,
+  domainName                     : 'mikebild.com',
+  hostName                       : 'cms-webapp',
 });
 
 app.run();
