602276bf0e5bee105d42ae1b53d1c465b90f0051
Mike Bild <mike.bild@googlemail.com>
2019-01-22 11:06:50 +0100

    feat(todo-app-example): CDK certificate and alias to todo web app

--- examples/todo-app/infrastructure/app.js
+++ examples/todo-app/infrastructure/app.js
@@ -2,18 +2,30 @@ const { App } = require('@aws-cdk/cdk');
 
 const API = require('../packages/todo-api/infrastructure/stack');
 const WebApp = require('../packages/todo-app/infrastructure/stack');
+const WebAppAlias = require('../packages/todo-app/infrastructure/dns-alias');
 const StorybookWebsite = require('../packages/todo-storybook/infrastructure/stack');
 
 const app = new App();
+const domainName = 'mikebild.com';
 
 const { apiUrl } = new API(app, 'todo-api');
 
-new WebApp(app, 'todo-app', {
-  apiUrl
+const webapp = new WebApp(app, 'todo-app', {
+  apiUrl,
+  domainName,
+  hostName: 'todo-webapp',
+  stageName: 'prod',
+});
+
+new WebAppAlias(app, 'todo-webapp-dns-alias', {
+  domainNameRegionalHostedZoneId: webapp.domainNameRegionalHostedZoneId,
+  domainNameRegionalDomainName: webapp.domainNameRegionalDomainName,
+  domainName,
+  hostName: 'todo-webapp',
 });
 
 new StorybookWebsite(app, 'todo-storybook', {
-  domainName: 'mikebild.com',
+  domainName,
   hostName: 'todo-storybook',
 });
 
--- examples/todo-app/packages/todo-app/infrastructure/stack.js
+++ examples/todo-app/packages/todo-app/infrastructure/stack.js
@@ -1,15 +1,24 @@
 const { join } = require('path');
 const { Stack, Output } = require('@aws-cdk/cdk');
 const { Function, Runtime, Code } = require('@aws-cdk/aws-lambda');
-const { LambdaRestApi, EndpointType } = require('@aws-cdk/aws-apigateway');
 const { PolicyStatement } = require('@aws-cdk/aws-iam');
+const {
+  LambdaRestApi,
+  EndpointType,
+  CfnDomainName,
+  CfnBasePathMapping,
+} = require('@aws-cdk/aws-apigateway');
+const { Certificate } = require('@aws-cdk/aws-certificatemanager');
+
 
 module.exports = class WebApp extends Stack {
   constructor(parent, id, props) {
     super(parent, id, props);
 
-    const lambda = new Function(this, 'todo-app-function', {
-      functionName: 'todo-app-function',
+    const domainName = `${props.hostName}.${props.domainName}`;
+
+    const lambda = new Function(this, 'todo-app', {
+      functionName: 'todo-app',
       runtime: Runtime.NodeJS810,
       handler: 'server.handler',
       code: Code.asset(join(__dirname, '../build')),
@@ -36,6 +45,57 @@ module.exports = class WebApp extends Stack {
 
     this.appUrl = new Output(this, 'AppUrl', {
       value: api.url,
+    })
+      .makeImportValue()
+      .toString();
+
+
+    /*
+    Attention: Manual confirmation by email!
+    */
+    const certificate = new Certificate(this, 'TodoWebAppCertificate', {
+      domainName
+    });
+
+    const domainname = new CfnDomainName(this, 'TodoWebAppDomainName', {
+      domainName,
+      endpointConfiguration: {
+        types: [
+          'REGIONAL',
+        ],
+      },
+      regionalCertificateArn: certificate.certificateArn,
+    });
+
+
+    new CfnBasePathMapping(this, 'TodoAppBasePathMapping', {
+      domainName: domainname.domainNameName,
+      stage: props.stageName,
+      restApiId: api.restApiId,
+    });
+
+    this.domainNameRegionalDomainName = new Output(
+      this,
+      'DomainNameRegionalDomainName',
+      {
+        value: domainname.domainNameRegionalDomainName,
+      }
+    )
+      .makeImportValue()
+      .toString();
+
+    this.domainNameRegionalHostedZoneId = new Output(
+      this,
+      'DomainNameRegionalHostedZoneId',
+      {
+        value: domainname.domainNameRegionalHostedZoneId,
+      }
+    )
+      .makeImportValue()
+      .toString();
+
+    this.domainNameName = new Output(this, 'DomainNameName', {
+      value: domainname.domainNameName,
     })
       .makeImportValue()
       .toString();
